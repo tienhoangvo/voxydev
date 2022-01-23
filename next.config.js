8a2110e4b5356fae43ae1c26448ffe928eb26a68
Tien Hoang Vo <tienhoangvo.dev@gmail.com>
2022-01-23 19:46:19 +0700

    trying to solve nextjs12 and next-pwa imcompality

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
-    dest: 'public',
-    runtimeCaching,
+    dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV !== 'production',
-    buildExcludes: [/middleware-manifest.json$/],
-  },
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/]
+  }
 });
