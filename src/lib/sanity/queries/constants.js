8812ce3191f5c7aa7c61cf58f39f1dae4a8a6f37
Tien Hoang Vo <tienhoangvo.dev@gmail.com>
2022-01-26 13:47:02 +0700

    increase the number of items per page

--- src/lib/sanity/queries/constants.js
+++ src/lib/sanity/queries/constants.js
@@ -1,5 +1,5 @@
-export const ARTICLE_PAGE_LIMIT = 2;
+export const ARTICLE_PAGE_LIMIT = 6;
 
-export const VIDEO_PAGE_LIMIT = 2;
+export const VIDEO_PAGE_LIMIT = 6;
 
-export const COMMENT_PAGE_LIMIT = 2;
+export const COMMENT_PAGE_LIMIT = 6;
